0a6b13daea99b25aa7a62cb7ad5e2c500710a732
JesterOlayvar219 <jester.olayvar.x@gmail.com>
2025-01-15 04:11:55 +0900

    initial

---
+++ remotion/Video.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AbsoluteFill } from "remotion";
+
+export const Video = () => {
+  return (
+    <AbsoluteFill>
+      <MyComp text="World" />
+    </AbsoluteFill>
+  );
+};
